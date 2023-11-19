0e64953590956e5ddd6e5c8196361c488740a69a
Yaroslav Muravitskiy <yarik77.97@mail.ru>
2023-11-19 12:45:33 +0300

    App code

--- opencv_test.py
+++ opencv_test.py
@@ -1,23 +1,13 @@
 import cv2
+img = cv2.imread("teddy.jpg")
+cv2.imshow("Click me", img)
+cv2.resizeWindow("Click me",1200,800)
 def show_rectandle(event, x, y, flags, params):
-    print("in function",k)
     if event==cv2.EVENT_LBUTTONDOWN:
-        cv2.rectangle(img,(x,y),(x+10,y+10),(0,0,0),10)
+        cv2.rectangle(img,(x,y),(x+15,y+15),(0,0,0),15)
         cv2.imshow("Click me", img)
-        return 0
-    else:
-        cv2.imshow("Click me", img)
-        return 0
-k=0
-print("before while",k)
-while(True):
-    print("in while",k)
-    img = cv2.imread("teddy.jpg")
-    cv2.imshow("Click me", img)
-    cv2.setMouseCallback("Click me",show_rectandle)
-    k+=1
-    print(k)
-    if k>=1:
-        break
+    elif event==cv2.EVENT_RBUTTONDOWN:
+        cv2.destroyAllWindows()
+cv2.setMouseCallback("Click me",show_rectandle)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
